ecb5dfb737ca504ae92572cc8340f18815dc8768
whopper-cemnl <wjhopper510@gmail.com>
2019-03-22 10:33:31 -0400

    give the adjustable bar a bounds property

--- AdjustableBar.py
+++ AdjustableBar.py
@@ -2,7 +2,7 @@ from psychopy.visual.rect import Rect
 
 
 class AdjustableBar(Rect):
-    def __init__(self, win, **kwargs):
+    def __init__(self, win, bounds=(-1, 1), **kwargs):
         # Initialize the rectangle
         super(AdjustableBar, self).__init__(win, **kwargs)
         # Create the "handle" point
@@ -10,6 +10,7 @@ class AdjustableBar(Rect):
                            lineColor="#000000", fillColor="#0000FF", lineWidth=1, autoDraw=True)
         self._focused = True
         self.adjusted = False
+        self._bounds = bounds
 
     @property
     def focused(self):
@@ -20,3 +21,22 @@ class AdjustableBar(Rect):
         if isFocused != self._focused:
             self._focused = isFocused
             self.handle.autoDraw = isFocused
+
+    @property
+    def bounds(self):
+        return self._bounds
+
+    @bounds.setter
+    def bounds(self, new_bounds):
+
+        if len(new_bounds) != 2:
+            raise ValueError("AdjustableBar boundaries must have 2 values")
+
+        for x in new_bounds:
+            if not isinstance(x, (int, float)):
+                raise ValueError("AdjustableBar boundaries must be a list or tuple of numeric values")
+
+        if isinstance(new_bounds, list):
+            new_bounds = tuple(new_bounds)
+
+        self._bounds = new_bounds
