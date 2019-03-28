133b4189235d5a8260b12951c7de13838ef99cf3
whopper-cemnl <wjhopper510@gmail.com>
2019-03-28 15:50:03 -0400

    make axis scale around arbirtary width, height and position

--- axis.py
+++ axis.py
@@ -2,33 +2,39 @@ from psychopy.visual import ShapeStim, TextStim, BufferImageStim
 from itertools import chain
 
 
-def AxisStim(win, y_labels=('A', 'B')):
+def AxisStim(win, pos=(0, 0), width=1.5, height=1, y_labels=("A", "B")):
 
-    y_axis = ShapeStim(win, vertices=[[-.75, -.5], [-.75, -.25], [-.775, -.25], [-.75, -.25],
-                                      [-.75, .25], [-.775, .25], [-.75, .25], [-.75, .5]],
+    # Boundaries in top, right, bottom, left order (like HTML/CSS)
+    bounds = (pos[1]+height/2, pos[0]+width/2, pos[1]-height/2, pos[0]-width/2)
+
+    y_axis = ShapeStim(win, vertices=[[bounds[3], bounds[2]], [bounds[3], bounds[2]+height/4],
+                                      [bounds[3]-.025, bounds[2]+height/4], [bounds[3], bounds[2]+height/4],
+                                      [bounds[3], bounds[0]-height/4], [bounds[3]-.025, bounds[0]-height/4],
+                                      [bounds[3], bounds[0]-height/4], [bounds[3], bounds[0]]],
                        lineColor="#00000", lineWidth=3)
 
-    x_axis_breaks = [round(.75 * (x / 5), 2) for x in range(-4, 6)]
+    x_axis_breaks = [round(bounds[3] + width * (x/10), 2) for x in range(1,11)]
     x_axis_break_vertices = list(zip(chain.from_iterable([[x, x, x] for x in x_axis_breaks]),
-                                     [-.5, -.525, -.5] * len(x_axis_breaks))
+                                     [bounds[2], bounds[2]-.025, bounds[2]] * len(x_axis_breaks))
                                  )
-    x_axis = ShapeStim(win, vertices=[(-.75, -.5)] + x_axis_break_vertices + [(.75, -.5)],
+    x_axis = ShapeStim(win, vertices=[(bounds[3], bounds[2])] + x_axis_break_vertices + [(bounds[1], bounds[2])],
                        lineColor="#00000", lineWidth=3, autoDraw=True)
 
     stimlist = [y_axis, x_axis]
 
-    for label, pos in zip(y_labels, [.25, -.25]):
-        t = TextStim(win, label, pos=(-.885, pos), height=.07, color="#00000")
+    for label, pos in zip(y_labels, [bounds[0]-height/4, bounds[2]+height/4]):
+        t = TextStim(win, label, pos=(bounds[3]-(.085*width), pos), height=height/15, color="#00000")
         stimlist.append(t)
 
-    for i, pos in enumerate([-.75] + x_axis_breaks):
-        t = TextStim(win, f'{(i / len(x_axis_breaks)) * 100:.0f}%', pos=(pos, -.55), height=.05, color="#00000")
+    for i, pos in enumerate([bounds[3]] + x_axis_breaks):
+        t = TextStim(win, f'{(i / len(x_axis_breaks)) * 100:.0f}%', pos=(pos, bounds[2]-.05), height=height/20, color="#00000")
         stimlist.append(t)
 
     x = BufferImageStim(win, stim=stimlist)
     # Width property will be useful for mapping bar length to probability (i.e., proportion of x axis it takes up)
-    x.width= 1.5  # -.75 to .75
-    x.height = 1  # -.5 to .5
-
+    x.width = width
+    x.height = height
+    x.bounds = bounds
     x.autoDraw = True
+
     return x
