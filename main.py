b658346e421fb7ff1a0d5cac8eeb65a0654d04b8
whopper-cemnl <wjhopper510@gmail.com>
2019-04-02 12:42:22 -0400

    fixed bar positioning when probabilities are different from .5

--- main.py
+++ main.py
@@ -6,9 +6,9 @@ from trials import static_trial, dynamic_trial
 win = visual.Window([1280, 768])
 event.globalKeys.add(key='q', func=core.quit, name='shutdown')
 
-# pA = .2
-# pB_given_A=.1
-# pB_given_notA = .95
+pA = .2
+pB_given_A=.1
+pB_given_notA = .95
 
 text = ["- {pA:.0f}% of people eat cookies.",
         "- {pB_given_A:.0f}% of people who eat cookies also eat brownies.",
@@ -16,10 +16,10 @@ text = ["- {pA:.0f}% of people eat cookies.",
         ]
 
 ax = AxisStim(win, pos=(0, .125), height=1, width=1, y_labels=('Eats\nCookies', "Doesn't Eat\nCookies"))  # autodraws
-responses = static_trial(ax, text)
+responses = static_trial(ax, text, pA=.2, pB_given_A=pB_given_A, pB_given_notA=pB_given_notA)
 print(responses)
 
-responses = dynamic_trial(ax, text)
+responses = dynamic_trial(ax, text, pB_given_A=pB_given_A, pB_given_notA=pB_given_notA)
 print(responses)
 
 win.close()
